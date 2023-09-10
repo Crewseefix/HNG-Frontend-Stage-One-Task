2c500573b2ae58268db5447db310f398f4f93cb3
Oluwatobi Bamidele <tostirma@gmail.com>
2023-09-10 11:17:17 +0100

    correct link

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const githubURL = "your_github_repository_url";
 
 document.getElementById("dayOfWeek").textContent = getCurrentDayOfWeek();
 document.getElementById("utcTime").textContent = getCurrentUTCTime();
-document.querySelector("[data-testid='githubURL'] a").href = githubURL;
 
 function getCurrentDayOfWeek() {
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
